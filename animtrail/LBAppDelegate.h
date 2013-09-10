a4078c6433ac696c6a3e64fec2776c1999b73f15
Ahmad Saifuddin <e.din0210@gmail.com>
2013-09-10 11:37:20 +0800

    Initial Commit

---
+++ animtrail/LBAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  LBAppDelegate.h
+//  animtrail
+//
+//  Created by saifuddin on 10/9/13.
+//  Copyright (c) 2013 saifuddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LBViewController;
+
+@interface LBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) LBViewController *viewController;
+
+@end
---
+++ animtrail/LBViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBViewController.h
+//  animtrail
+//
+//  Created by saifuddin on 10/9/13.
+//  Copyright (c) 2013 saifuddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBViewController : UIViewController
+
+@end
